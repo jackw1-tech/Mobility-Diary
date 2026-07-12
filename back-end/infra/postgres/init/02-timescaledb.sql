48d53fcc64de36dfb1c8c64580084729ad1a8eae
jackw1-tech <107569512+jackw1-tech@users.noreply.github.com>
2026-07-12 18:50:09 +0200

    small fix timescale production

--- back-end/infra/postgres/init/02-timescaledb.sql
+++ back-end/infra/postgres/init/02-timescaledb.sql
@@ -1,1 +1,11 @@
-CREATE EXTENSION IF NOT EXISTS timescaledb;
+DO $$
+BEGIN
+    IF EXISTS (
+        SELECT 1
+        FROM pg_available_extensions
+        WHERE name = 'timescaledb'
+    ) THEN
+        EXECUTE 'CREATE EXTENSION IF NOT EXISTS timescaledb';
+    END IF;
+END
+$$;
